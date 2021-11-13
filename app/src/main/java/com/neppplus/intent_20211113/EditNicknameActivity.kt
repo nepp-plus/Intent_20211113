65b13b845fcad6deb09800866c2e9041528df5bf
nepp-plus <nepp.plus@gmail.com>
2021-11-13 16:21:19 +0900

    변경 확인 -> 메인 복귀  - 입력값 변수 저장  - 결과데이터 첨부용 Intent / putExtra  - RESULT_OK,  Intent 를 결과로 설정  - finish()

--- app/src/main/java/com/neppplus/intent_20211113/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211113/EditNicknameActivity.kt
@@ -1,11 +1,32 @@
 package com.neppplus.intent_20211113
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        btnOk.setOnClickListener {
+
+//            1. 입력한 값을 변수에 저장
+            val inputNickname  =  edtNickname.text.toString()
+
+//            2. 새 닉네임 들고 복귀
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", inputNickname)
+
+//            3. 결과 설정 - OK 누른게 맞다고 구별,  들고 갈 데이터
+            setResult(RESULT_OK, resultIntent)
+
+//            4. 실제 복귀
+            finish()
+
+
+        }
+
     }
 }
